d6cebe71245483fe51e5b2cb406a625aa8979674
Eric <ericos.bennett@gmail.com>
2021-01-27 22:08:02 +0000

    reconfigure load tweets function with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,48 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 /* eslint-env jquery */
 /* eslint-env browser */
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(() => {
-
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "createdAt": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "createdAt": 1611685701267
-    }
-  ];
-
-  const renderTweets = function(tweets) {
-    $.each(tweets, (i, v) => {
-      $('#tweets-container').prepend(createTweetElement(v));
-    });
-    
-  };
-
+  
   const createTweetElement = function(tweet) {
     let $tweet = $(`
       <article class="tweet">
@@ -64,24 +24,42 @@ $(document).ready(() => {
     `);
     return $tweet;
   };
+  
+  const renderTweets = function(tweets) {
+    $.each(tweets, (i, v) => {
+      $('#tweets-container').prepend(createTweetElement(v));
+    });
+  };
+  
+  const loadTweets = () => {
 
-  renderTweets(tweetData);
+    $.ajax('../tweets', {
+      method: 'GET'
+    }).then(result => {
+      renderTweets(result);
+    }).catch(err => {
+      console.log(err);
+    });
 
+  };
+
+  // Tweet submission handler
   $('.new-tweet form').on('submit', function(event) {
     
     event.preventDefault();
 
-    console.log($(this).serialize());
-
     $.ajax('../tweets', {
       method: 'POST',
       data: $(this).serialize()
-    }).then(function(response) {
-      console.log(response);
-    }).catch(function(err) {
+    }).then(() => {
+      loadTweets();
+    }).catch(err => {
       console.log(err);
     });
 
   });
+
+  // Load tweets on page load
+  loadTweets();
 
 });
